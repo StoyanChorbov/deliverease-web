0ad44017bb2355fe7a7bbbc0aabdeef059c18382
StoyanChorbov <slazy06bg@gmail.com>
2025-05-27 09:53:37 +0300

    Test

--- View/nuxt.config.ts
+++ View/nuxt.config.ts
@@ -6,7 +6,15 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     app: {
-        baseURL: '/public',
+        baseURL: '/',
+    },
+    nitro: {
+        publicAssets: [
+            {
+                dir: 'public',
+                baseURL: '/'
+            }
+        ]
     },
     build: {
         transpile: ['vuetify'],
